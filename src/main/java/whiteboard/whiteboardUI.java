d6da2c086636a4b1bf3cf9db84a4472519453c9e
Sebastian Bobadilla <sebitasgon@gmail.com>
2022-10-04 20:59:48 +1100

    Initial commit

---
+++ src/main/java/whiteboard/whiteboardUI.java
@@ -0,0 +1,15 @@
+package whiteboard;
+
+import javax.swing.*;
+
+public class whiteboardUI {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JButton btn_line;
+    private JButton btn_rectangle;
+    private JButton btn_circle;
+    private JButton btn_triangle;
+    private JButton btn_free;
+    private JButton btn_text;
+    private JComboBox comboBox2;
+}
