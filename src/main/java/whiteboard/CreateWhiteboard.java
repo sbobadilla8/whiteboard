dd2e6a0fb2ba333e8ecb16a3519f26f40f15328f
Suhrid Gupta <suhridg@student.unimelb.edu.au>
2022-10-07 04:48:57 -0700

    Changed name to CreateWhiteboard.java

--- src/main/java/whiteboard/CreateWhiteboard.java
+++ src/main/java/whiteboard/CreateWhiteboard.java
@@ -2,7 +2,7 @@ package whiteboard;
 
 import javax.swing.*;
 
-public class Whiteboard {
+public class CreateWhiteboard {
     public static void main (String[] args) {
         JFrame frame = new whiteboardUI("Whiteboard");
         frame.setVisible(true);
